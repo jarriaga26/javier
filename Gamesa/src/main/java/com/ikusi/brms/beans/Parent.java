68b5ba3219fda9003997fa1d37f661b4fe0b065f
jvazquez2drools <>
2016-02-15 15:48:21 -0600

    {/Gamesa/src/main/java/com/ikusi/brms/beans/Parent.java}

--- Gamesa/src/main/java/com/ikusi/brms/beans/Parent.java
+++ Gamesa/src/main/java/com/ikusi/brms/beans/Parent.java
@@ -9,8 +9,27 @@ public class Parent implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
+   @org.kie.api.definition.type.Label(value = "sysId")
+   @XStreamAlias("sys_id")
+   private java.lang.String sysId;
+
    public Parent()
    {
+   }
+
+   public java.lang.String getSysId()
+   {
+      return this.sysId;
+   }
+
+   public void setSysId(java.lang.String sysId)
+   {
+      this.sysId = sysId;
+   }
+
+   public Parent(java.lang.String sysId)
+   {
+      this.sysId = sysId;
    }
 
 }
